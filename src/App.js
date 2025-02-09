6e5f8a964278ef31a7945984e8a93823737d61d1
metharishirisha <metharishirisha7@gmail.com>
2025-02-09 10:11:48 +0000

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import SeasonProfile from './components/BannerCardItem/index'
+import BannerCardItem from './components/BannerCardItem'
+
 import './App.css'
 
 const bannerCardsList = [
@@ -24,10 +25,10 @@ const bannerCardsList = [
 ]
 
 const App = () => (
-  <div>
-    <ul>
-      {bannerCardsList.map(eachItem => (
-        <SeasonProfile bannerCards={eachItem} key = {eachItem.id}/>
+  <div className="app-container">
+    <ul className="banner-cards-list">
+      {bannerCardsList.map(eachBanner => (
+        <BannerCardItem bannerDetails={eachBanner} key={eachBanner.id} />
       ))}
     </ul>
   </div>
--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -1,14 +1,20 @@
-const SeasonProfile = props => {
-  const {bannerCards} = props
-  const {headerText, description, className} = bannerCards
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerDetails} = props
+  const {headerText, description, className} = bannerDetails
 
   return (
-    <li className={`${className} card-item`}>
-      <h1>{headerText}</h1>
-      <p>{description}</p>
-      <button>Show More</button>
+    <li className={`${className} banner-card-item`}>
+      <div>
+        <h1 className="heading">{headerText}</h1>
+        <p className="description">{description}</p>
+        <button className="show-more-btn" type="button">
+          Show More
+        </button>
+      </div>
     </li>
   )
 }
 
-export default SeasonProfile
+export default BannerCardItem
